1e677e315352cfa9ac89ab71bb8bf0a52d6af0f8
Kabiir <kabiirk@gmail.com>
2021-12-09 01:48:53 +0530

    Day 8 Soln.

--- day8.py
+++ day8.py
@@ -22,6 +22,58 @@ for entry in entries:
 print(unique_digit_count)
 
 # Part 2
+def find_nums(left_hand_values):
+    nums = dict()
+    segments = dict()
+
+    uniques = {1:2,7:3,4:4,8:7}
+    for num,len_num in uniques.items():
+        nums[num] = [x for x in left_hand_values if len(x) == len_num].pop()
+
+    segments[1] = (set(nums[7]) - set(nums[1])).pop()
+
+    nums[9] = [x for x in left_hand_values if len(x) == 6 and set(nums[4] + segments[1]) < set(x)].pop()
+    segments[7] = (set(nums[9]) - set(nums[4] + segments[1])).pop()
+
+    nums[3] = [x for x in left_hand_values if len(x) == 5 and set(nums[7] + segments[7]) < set(x)].pop()
+    segments[4] = (set(nums[3]) - set(nums[7]+segments[7])).pop()
+    segments[2] = (set(nums[9]) - set(nums[3])).pop()
+
+    nums[5] = [x for x in left_hand_values if len(x) == 5 and set(segments.values()) < set(x)].pop()
+    segments[6] = (set(nums[5]) - set(segments.values())).pop()
+    
+    nums[6] = [x for x in left_hand_values if len(x) == 6 and set(segments.values()) < set(x) and x != nums[9]].pop()
+    segments[5] = (set(nums[6]) - set(segments.values())).pop()
+    segments[3] = (set(nums[8]) - set(segments.values())).pop()
+    
+    nums[2] = ''.join(segments[x] for x in [1,3,4,5,7])
+    nums[0] = ''.join(segments[x] for x in [1,2,3,5,6,7])
+
+    return {''.join(sorted(value)):key for key,value in nums.items()}
+
+def get_output_values(right_hand_values, nums):
+    output_num = 0
+    for index, value in enumerate(right_hand_values[-1::-1]):
+        value_sorted = ''.join(sorted(value))
+        output_num += nums[value_sorted] * (10**index)
+    return output_num
+
+def main():
+    with open('input8.txt','r') as infile:
+        display_values = [(sides[0].split(),sides[1].split()) for sides in (line.strip().split(" | ") for line in infile.readlines())]
+
+    total = 0
+
+    for entry in display_values:
+        left_hand_values, right_hand_values = entry
+        nums = find_nums(left_hand_values)
+        output_value = get_output_values(right_hand_values,nums)
+        total+=output_value
+
+    print(total)
+
+main()
+
 class seven_seg_display:
     display = {
         'a':False,
@@ -39,7 +91,7 @@ class seven_seg_display:
         for letter in string:
             self.display[letter] = True
 
-    def get_number(self):
+    def get_number(self, string):
         # Mapping :
         #
         #   dddd
@@ -49,59 +101,56 @@ class seven_seg_display:
         #  g    b
         #  g    b
         #   cccc
+        self.set_display(string)
+        # Unique numbers
+        if(len(string) == 2):
+            return '1'
+        if(len(string) == 4):
+            return '4'
+        if(len(string) == 3):
+            return '7'
+        if(len(string) == 7):
+            return '8'
 
+        a = self.display['a']
+        b = self.display['b']
+        c = self.display['c']
+        d = self.display['d']
+        e = self.display['e']
+        f = self.display['f']
+        g = self.display['g']
         # 0
-        if(self.display['d'] and self.display['a'] and\
-            self.display['b'] and self.display['c'] and\
-            self.display['g'] and self.display['e']
-            ):
+        if( a and b and c and d and e and not( f) and g ):
             return '0'
         # 1
-        if(self.display['a'] and self.display['b']):
-            return '1'
+        # if( a and b and not c and not d and not e and not f and not g ):
+        #     return '1'
         # 2
-        if(self.display['d'] and self.display['a'] and\
-            self.display['f'] and self.display['g'] and\
-            self.display['c'] ):
+        if( a and not( b )and c and d and not( e) and f and g ):
             return '2'
         # 3
-        if(self.display['d'] and self.display['a'] and\
-            self.display['f'] and self.display['b'] and\
-            self.display['c'] ):
+        if( a and b and c and d and not( e) and f and not( g) ):
             return '3'
         # 4
-        if(self.display['e'] and self.display['f'] and\
-            self.display['a'] and self.display['b'] ):
-            return '0'
+        # if( a and b and not c and not d and e and f and not g ):
+        #     return '4'
         # 5
-        if(self.display['d'] and self.display['e'] and\
-            self.display['f'] and self.display['b'] and\
-            self.display['c'] ):
+        if( not( a) and b and c and d and e and f and not( g) ):
             return '5'
         # 6
-        if(self.display['d'] and self.display['e'] and\
-            self.display['g'] and self.display['c'] and\
-            self.display['b'] and self.display['f']):
+        if( not(a) and b and c and d and e and f and g ):
             return '6'
         # 7
-        if(self.display['d'] and self.display['a'] and\
-            self.display['b'] ):
-            return '7'
+        # if( a and b and c and d and not e and not f and not g ):
+        #     return '7'
         # 8
-        if(self.display['d'] and self.display['a'] and\
-            self.display['b'] and self.display['c'] and\
-            self.display['g'] and self.display['e'] and\
-            self.display['f']
-            ):
-            return '8'
+        # if( a and b and c and d and e and f and g ):
+        #     return '8'
         # 9
-        if(self.display['d'] and self.display['a'] and\
-            self.display['e'] and self.display['f'] and\
-            self.display['a'] and self.display['c'] and \
-            self.display['b'] ):
+        if( a and b and c and d and e and f and not(g) ):
             return '9'
 
-        return f'{self.display} No digit Set !'
+        return '1'
 
     def clear_display(self):
         for key in self.display:
@@ -110,17 +159,25 @@ class seven_seg_display:
 display = seven_seg_display()
 
 decoded = []
-# for entry in entries:
-#     outputs = entry[1]
-#     code = ''
-#     for output in outputs:
-#         display.set_display(output)
-#         code += display.get_number()
-#         display.clear_display()
-#     print(code)
+final_sum = 0
+for entry in entries:
+    outputs = entry[1]
+    code = ''
+    for output in outputs:
+        code += display.get_number(output)
+        display.clear_display()
+    final_sum += int(code)
+print(final_sum) # 1091165
 
-op = ['fdgacbe', 'cefdb', 'cefbgd', 'gcbe']
-for o in op:
-    display.set_display(o)
-    print(display.get_number())
-    display.clear_display()
+# op = ['fdgacbe', 'cefdb', 'cefbgd', 'gcbe']
+# code = ''
+
+# s = 'fcgedb'
+# print(display.get_number(s))
+# print(display.display)
+# display.clear_display()
+
+# for o in op:
+#     code += display.get_number(o)
+#     display.clear_display()
+# print(code)
