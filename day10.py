50526acbe3ba5113f9baf075a58bc065780c148e
Kabiir <kabiirk@gmail.com>
2021-12-12 00:05:31 +0530

    Day 10 and Day 11 Soln.

--- day10.py
+++ day10.py
@@ -1,20 +1,77 @@
 sub_system  = []
 with open('input10.txt', 'r') as f:
     for line in f:
+        # because strings re immutable in python
+        # line =  line.replace('{', 'a')
+        # line = line.replace('}', 'b')
         sub_system.append( line.strip() )
 
-print(sub_system)
+#print(sub_system)
 
-closing_chunk = {
+closing_bracket = {
     '(' : ')',
     '[' : ']',
-    '{' : '}',
+    'a' : 'b',
     '<' : '>',
 }
 
 score = {
     ')' : 3,
     ']' : 57,
-    '}' : 1197,
+    'b' : 1197,
     '>' : 25137
 }
+
+def first_illegal_character( code_line ):
+    '''
+    Using list as stack where :
+    [a, b, c, d, e, f, g]
+     ^                 ^
+    Bottom            Top
+    '''
+
+    illegals = []
+    opening_bracket = []
+    n = len(code_line)
+    for i in range(n):
+        if(code_line[i] in ['(', '[', 'a', '<'] ):
+            # Add to top
+            opening_bracket.append(code_line[i])
+        elif(code_line[i] == closing_bracket[opening_bracket[-1]]):
+            # remove from top
+            opening_bracket.pop()
+    return opening_bracket
+
+s = '{([(<{}[<>[]}>{[]{[(<()>'
+s = '[[<[([]))<([[{}[[()]]]'
+s = s.replace('{', 'a')
+s = s.replace('}', 'b')
+# print(s)
+# print(first_illegal_character(s))
+
+from collections import deque
+import fileinput
+
+pairs = {"(": ")", "[": "]", "{": "}", "<": ">"}
+points = {")": 3, "]": 57, "}": 1197, ">": 25137}
+part1, part2 = 0, []
+
+for line in sub_system:
+    stack = deque()
+    for c in line.strip():
+        if c in "([{<":
+            stack.appendleft(pairs[c])
+        elif c != stack.popleft():
+            part1 += points[c]
+            break
+    else:
+        score = 0
+        for c in stack:
+            score = score * 5 + ")]}>".index(c) + 1
+        part2.append(score)
+
+# Part 1
+print(part1) # 387363
+
+# Part 2
+print(sorted(part2)[len(part2) // 2]) # 4330777059
---
+++ day11.py
@@ -0,0 +1,40 @@
+real_input = open("input11.txt").read().splitlines()
+grid = {(m, n): int(c) for m, line in enumerate(real_input) for n, c in enumerate(line)}
+
+def inc_neighbours(grid, m, n):
+    neighbour_incs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    for dm, dn in neighbour_incs:
+        if (m+dm, n+dn) in grid:
+            grid[(m+dm, n+dn)] += 1
+
+def flash(grid, already_flashed):
+    next_grid = grid.copy()
+    while True:
+        flashed_now = set()
+        for ind, v in grid.items():
+            if v > 9 and ind not in already_flashed:
+                flashed_now.add(ind)
+                inc_neighbours(next_grid, *ind)
+        if not flashed_now:
+            break
+        grid = next_grid
+        already_flashed |= flashed_now
+        yield already_flashed, next_grid
+
+def step(grid):
+    already_flashed = set()
+    for ind in grid: grid[ind] += 1
+    for already_flashed, grid in flash(grid, already_flashed): pass
+    for ind in already_flashed: grid[ind] = 0
+    return len(already_flashed), grid
+
+total = 0
+for i in range(1000):
+    flashes, grid = step(grid)
+    total += flashes
+    if i == 99:
+        # Part 1
+        print(total) # 1739
+    if flashes == len(grid):
+        print(i+1) # 324
+        break
