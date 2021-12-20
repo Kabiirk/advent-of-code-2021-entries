9ca2480a7db04e3cfa6098ce1ff92bdcab136650
Kabiir <kabiirk@gmail.com>
2021-12-20 23:11:38 +0530

    Day 19 boilerplate and Part1 README.md filled

--- Day19/Day19.cpp
+++ Day19/Day19.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+void readFile(string filename){
+    string line;
+    ifstream myfile (filename);
+    if (myfile.is_open())
+    {
+        while ( getline(myfile, line) )
+        {
+            cout<<line<<endl;
+        }
+        myfile.close();
+    }
+    else cout << "Unable to open file";
+
+}
+
 int main() {
-    cout<<"Hello World!"<<endl;
+    readFile("input.txt");
 
     return 0;
 }
