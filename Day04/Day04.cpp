edb60ca58a34678dab3705323718f641b0960e9b
Kabiir <kabiirk@gmail.com>
2022-01-04 00:50:09 +0530

    Still parsing input.txt properly

--- Day04/Day04.cpp
+++ Day04/Day04.cpp
@@ -9,15 +9,20 @@ using namespace std;
 
 
 /*
+< nums >
 <
-< nums >,
-< <r><c> >
+    < <Board 1 row 1>, <Board 1 Row 2> ... <Board 1 Row 5> >,
+    < <Board 2 row 1>, <Board 2 Row 2> ... <Board 2 Row 5> >,
+    < <Board 3 row 1>, <Board 3 Row 2> ... <Board 3 Row 5> >,
+    .....
+    < <Board n row 1>, <Board n Row 2> ... <Board n Row 5> >,
 >
 */
 void readFile(string filename){
     vector<string> full_data;
     vector<int> line_data;
     int linecount = 1;
+    int rowcount = 0;
     string line;
     ifstream myfile (filename);
     int blank = 0;
@@ -30,23 +35,24 @@ void readFile(string filename){
                 //cout<<"blank !"<<endl;
                 blank++;
             }
+            else if(line.length() > 14){
+                blank++;
+            }
             else{
                 const regex r("(\\d*)(\\s*)(\\d*)(\\s*)(\\d*)(\\s*)(\\d*)(\\s*)(\\d*)");
                 smatch result;
                 if(regex_search(line, result, r)){
                     //cout<<line<<endl;
-                    cout<<result[1]<<"->"<<result[3]<<"->"<<result[5]<<"->"<<result[7]<<"->"<<result[9]<<endl;
+                    if(rowcount<=4)
+                    {
+                        cout<<result[1]<<"->"<<result[3]<<"->"<<result[5]<<"->"<<result[7]<<"->"<<result[9]<<endl;
+                        rowcount++;
+                    }
+                    else{
+                        cout<<endl;
+                        rowcount = 0;
+                    }
                 }
-                // if(linecount == 1){
-                //     //stringstream ss (line);
-                //     cout<<"Top Line !!"<<endl;
-                //     linecount++;
-                // }
-                // else{
-                //     stringstream ss (line);
-                //     cout<<line<<endl;
-                // }
-                //full_data.push_back(line);
             }
         }
         myfile.close();
