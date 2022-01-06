78db7edca6e2f45d7a23e2163c6a9350f2548d66
Kabiir <kabiirk@gmail.com>
2022-01-06 21:01:31 +0530

    Succsessfully Parsed input with regex

--- Day04/Day04.cpp
+++ Day04/Day04.cpp
@@ -7,20 +7,23 @@
 
 using namespace std;
 
-
-/*
-< nums >
-<
-    < <Board 1 row 1>, <Board 1 Row 2> ... <Board 1 Row 5> >,
-    < <Board 2 row 1>, <Board 2 Row 2> ... <Board 2 Row 5> >,
-    < <Board 3 row 1>, <Board 3 Row 2> ... <Board 3 Row 5> >,
-    .....
-    < <Board n row 1>, <Board n Row 2> ... <Board n Row 5> >,
->
-*/
 void readFile(string filename){
+    /*
+    Desired Vector shapes :
+    nums = < 1, 2, 3, 4 .... >
+
+    boards = 
+    <
+        < <Board 1 row 1>, <Board 1 Row 2> ... <Board 1 Row 5> >,
+        < <Board 2 row 1>, <Board 2 Row 2> ... <Board 2 Row 5> >,
+        < <Board 3 row 1>, <Board 3 Row 2> ... <Board 3 Row 5> >,
+        .....
+        < <Board n row 1>, <Board n Row 2> ... <Board n Row 5> >,
+    >
+    */
     vector<vector<int>> full_data;
     vector<int> line_data;
+    vector<int> nums;
     string line;
     ifstream myfile (filename);
     int blank = 0;
@@ -34,7 +37,18 @@ void readFile(string filename){
                 blank++;
             }
             else if(line.length() > 14){
-                blank++;
+                stringstream ss(line);
+
+                while(ss.good()){
+                    string substr;
+                    getline(ss, substr, ',');
+                    nums.push_back(stoi(substr));
+                }
+
+                for(auto t : nums){
+                    cout<<t+9<<", ";
+                }
+
             }
             else{
                 const regex r("(\\s*)(\\d*)(\\s*)(\\d*)(\\s*)(\\d*)(\\s*)(\\d*)(\\s*)(\\d*)");
@@ -42,7 +56,7 @@ void readFile(string filename){
                 if(regex_search(line, result, r)){
                     //cout<<line<<endl;
                     //cout<<result[1]<<"->"<<result[3]<<"->"<<result[5]<<"->"<<result[7]<<"->"<<result[9]<<endl;
-                    // TODO : Find a faster way to do this
+                    // TODO : Find a faster/better way to do this
                     line_data.push_back(stoi(result[2]));
                     line_data.push_back(stoi(result[4]));
                     line_data.push_back(stoi(result[6]));
@@ -61,8 +75,23 @@ void readFile(string filename){
     }
     else cout << "Unable to open file";
 
-    // Re-arranging the full_data to desired shape
-    vector< vector< vector<int> > > board = { { { 1,2,3,4,5}, { 1,2,3,4,5}} };
+    // Re-arranging full_data into Desired Vector shape
+    vector< vector< vector<int> > > boards;
+    int track = 0;
+
+    while( full_data.size() > 0 ){
+        vector<vector<int>> current_board;
+        while(track < 5){
+            vector<int> current_line;
+            current_line = *full_data.begin();
+            current_board.push_back(current_line);
+            // operation similar to pop_front() for vectors
+            full_data.erase(full_data.begin());
+            track++;
+        }
+        track = 0;
+        boards.push_back(current_board);
+    }
 
     //return report;
 }
