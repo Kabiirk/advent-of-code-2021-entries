d2ed2a3b2cf86281a7d656cffded0d22ee38981d
Kabiir <kabiirk@gmail.com>
2022-02-08 02:47:07 +0530

    Day 9 parsing input.txt complete

--- Day09/Day09.cpp
+++ Day09/Day09.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
+#include "../utils/utils.h"
 
 using namespace std;
 
-void readFile(string filename){
+vector<vector<int>> readFile(string filename){
+    vector<vector<int>> basing_heights;
     string line;
     ifstream myfile (filename);
 
     if (myfile.is_open()){
         while ( getline(myfile, line) ){
-            cout<<line<<endl;
+            vector<int> current_row;
+            int n = line.length();
+            for(int i = 0; i<n; i++){
+                current_row.push_back(line[i]-'0');
+            }
+
+            basing_heights.push_back(current_row);
         }
     }
     else cout << "Unable to open file";
+
+    return basing_heights;
 }
 
 int main() {
-    readFile("input.txt");
+    vector<vector<int>> basin_heights = readFile("input.txt");
 
     return 0;
 }
