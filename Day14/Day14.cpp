8eb3a80e9baed8e809e1d3da0f53ff069d789b38
Kabiir <kabiirk@gmail.com>
2022-02-15 00:26:17 +0530

    Day14.py created + Day14.cpp in progress

--- Day14/Day14.cpp
+++ Day14/Day14.cpp
@@ -5,15 +5,48 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include "../utils/utils.h"
 
 typedef unsigned long long int ULLI;
 
 using namespace std;
 
-void printMap( map<string, string> dictionary ){
-    for(auto it = dictionary.cbegin(); it != dictionary.cend(); ++it){
-        cout << it->first << " " << it->second << "\n";
+struct RequiredData {
+    string polymer;
+    map<string, string> rules;
+};
+
+RequiredData readFile(string filename){
+    string line;
+    string to_append;
+    string to_match;
+    RequiredData req_data;
+
+    // Read File
+    ifstream myfile ("input.txt");
+    if (myfile.is_open())
+    {
+        while ( getline(myfile, line) )
+        {
+            if(line.length() > 10){
+                req_data.polymer = line;
+            }
+            else if(line.length() == 0){
+                // Blank line
+                continue;
+            }
+            else{
+                to_append = line.back();
+                to_match = line.substr(0, 2);
+                // cout << to_match << " ==> " << to_append << endl;
+                req_data.rules.insert( pair<string, string>(to_match, to_append) );
+            }
+        }
+        myfile.close();
     }
+    else cout << "Unable to open file";
+
+    return req_data;
 }
 
 string extendPolymer( map<string, string> rules, string pattern, int iterations){
@@ -62,45 +95,73 @@ ULLI calcMinMaxFrequencyDifference(map<char, ULLI> frequency_dict){
     return max_value-min_value;
 }
 
-int main() {
-    string line, pattern;
-    string to_append;
-    string to_match;
-    map<string, string> rules;
+void printInsertionPairs(map<string, vector<string>> post_insertion_pairs){
+    for(auto item : post_insertion_pairs){
+        cout<<item.first<<" : ";
+        printVector(item.second);
+    }
+}
 
-    // Read File
-    ifstream myfile ("input.txt");
-    if (myfile.is_open())
-    {
-        while ( getline(myfile, line) )
-        {
-            if(line.length() > 10){
-                pattern = line;
-            }
-            else if(line.length() == 0){
-                // Blank line
-                continue;
-            }
-            else{
-                to_append = line.back();
-                to_match = line.substr(0, 2);
-                // cout << to_match << " ==> " << to_append << endl;
-                rules.insert( pair<string, string>(to_match, to_append) );
+void populatePairInsertions(map<string, string> rules, map<string, vector<string>> &post_insertion_pairs){
+    for(auto item : rules){
+        // item= pair<HK, F>
+        vector<string> insertions;
+        auto insert1 = std::string(1,item.first[0]) + item.second;
+        auto insert2 = item.second + std::string(1,item.first[1]);
+        insertions.push_back(insert1);
+        insertions.push_back(insert2);
+        post_insertion_pairs[item.first] = insertions;
+    }
+}
+
+map<string, int> applySteps(string polymer, map<string, vector<string>> templates, map<string, int> polymers, int steps){
+    for(int step=0; step<steps; step++){
+        map<string, int> step_polymers(polymers);
+        // pol = pair<"HK" : integer>
+        for(auto pol : step_polymers){
+            if(pol.second > 0){
+                vector<pair<string, vector<string>>> temp_templates;
+                for(auto t : templates){
+                    if(t.first == pol.first){
+                        temp_templates.push_back(t);
+                    }
+                }
+                // temp_template = pair<"HK" : < "HF", "FK" >>
+                pair<string, vector<string>> temp_template = temp_templates[0];
+                polymers[temp_template.second[0]]+=pol.second;
+                polymers[temp_template.second[1]]+=pol.second;
+                polymers[pol.first] -= pol.second;
             }
         }
-        myfile.close();
     }
-    else cout << "Unable to open file";
+}
+
+int main() {
+    RequiredData req_data = readFile("input.txt");
+    string polymer = req_data.polymer;
+    map<string, string> rules = req_data.rules;
+    map<string, vector<string>> templates;
+    map<string, int> polymers;
+
+    populatePairInsertions(rules, templates);
+    // printInsertionPairs(post_insertion_pairs);
+    for(auto item : templates){
+        polymers[item.first] = 0;
+        polymers[item.second[0]] = 0;
+        polymers[item.second[1]] = 0;
+    }
+    applySteps(polymer, templates, polymers, 40);
+
 
     // Part 1
-    string final_pattern = extendPolymer(rules, pattern, 10);
-    map<char, ULLI> frequency_dict = string2Frequency(final_pattern);
-    cout << calcMinMaxFrequencyDifference(frequency_dict) << endl; // 3555
+    // string final_pattern = extendPolymer(rules, polymer, 10);
+    // map<char, ULLI> frequency_dict = string2Frequency(final_pattern);
+    // cout << calcMinMaxFrequencyDifference(frequency_dict) << endl; // 3555
 
     // Part 2
-    string final_pattern2 = extendPolymer(rules, pattern, 40);
-    map<char, ULLI> frequency_dict2 = string2Frequency(final_pattern2);
-    cout<< calcMinMaxFrequencyDifference(frequency_dict2) << endl; // 4439442043739
+    // string final_pattern2 = extendPolymer(rules, pattern, 40);
+    // map<char, ULLI> frequency_dict2 = string2Frequency(final_pattern2);
+    // cout<< calcMinMaxFrequencyDifference(frequency_dict2) << endl; // 4439442043739
 
     return 0;
 }
---
+++ Day14/Day14.py
@@ -0,0 +1,63 @@
+import os
+import sys
+from collections import Counter, defaultdict
+
+data = ""
+
+def getData(fileName):
+    strPath = os.path.join(sys.path[0], fileName)
+    with open(strPath) as fileObject:
+        data = fileObject.read().split('\n')
+
+    return data
+
+def applySteps(polymers, steps):
+    for step in range(steps):
+        stepPolymers = polymers.copy()
+        for pol in [p for p in stepPolymers.items() if p[1] > 0]:
+            template = [t for t in templates if t["pair"] == pol[0]][0]
+            polymers[template["insertion"][0]] += pol[1]
+            polymers[template["insertion"][1]] += pol[1]
+            polymers[pol[0]] -= pol[1]
+    singles = defaultdict(list)
+
+    for pol in polymers.items():
+        key, value = pol
+        if not key[0] in singles:
+            singles[key[0]] = 0
+        singles[key[0]] += value
+        if not key[1] in singles:
+            singles[key[1]] = 0
+        singles[key[1]] += value
+        if key[0] == key[1]:
+            singles[key[0]] += 1
+    singles[polymer[0]] += 1
+    singles[polymer[-1]] += 1
+    print(singles);
+
+    mostCommon = max(singles.values())
+    leastCommon = min(singles.values())
+    print((mostCommon - leastCommon)//2)
+
+    return polymers
+
+
+data = ''
+with open('input.txt') as fileObject:
+    data = fileObject.read().split('\n')
+
+polymer = data[0]
+
+templates = [{"pair": p[0].strip(), "insertion": [p[0][0] + p[1].strip(), p[1].strip() + p[0][1]]} for p in [i.split('->') for i in data if '->' in i]]
+polymers = defaultdict(list)
+
+for template in templates:
+    polymers[template["pair"]] = 0
+
+for index in range(len(polymer)-1):
+    polymers[polymer[index:index+2]] += 1
+
+# Part 1
+polymers = applySteps(polymers, 10)
+# Part 2
+applySteps(polymers, 30)
