8030cf948c6d821da1a8ea395e10723c3df98bd1
Kabiir <kabiirk@gmail.com>
2022-02-23 00:56:54 +0530

    Day19.cpp WIP

--- Day18/Day18.cpp
+++ Day18/Day18.cpp
@@ -2,20 +2,19 @@
 #include <fstream>
 #include <vector>
 #include <string>
-
-typedef int ULLI;
+#include "../utils/utils.h"
 
 using namespace std;
 
-vector<ULLI> readFile(string filename){
-    vector<ULLI> file_output;
+vector<string> readFile(string filename){
+    vector<string> file_output;
     string line;
     ifstream myfile (filename);
     if (myfile.is_open())
     {
         while ( getline(myfile, line) )
         {
-            cout<<line<<endl;
+            file_output.push_back(line);
         }
         myfile.close();
     }
@@ -24,17 +23,8 @@ vector<ULLI> readFile(string filename){
     return file_output;
 }
 
-void add(){
-}
-
-void explode(){
-}
-
-void split(){
-}
-
 int main() {
-    vector<ULLI> blowfish_numbers = readFile("input.txt");
+    vector<string> blowfish_numbers = readFile("input.txt");
 
     return 0;
 }
