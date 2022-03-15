116bb122e4e589e571372fc5c1dcd2aa21d0b570
Kabiir <kabiirk@gmail.com>
2022-03-16 00:04:30 +0530

    Code cleanup + instruction on how to run

--- compile.sh
+++ compile.sh
@@ -1,15 +1,24 @@
+# compile.sh only required for C++ solutions
+# For Python Solutions just simply type 
+# bash run.sh XX py
+
+# How to Run :
+# bash compile.sh XX
+
+# where XX => 01, 02, 03 ... 23, 24, 25
+
+# E.g. :
+# 1.
+# bash compile.sh 01
+# Compiles the C++ code for Day01
+
 if [ $# -ne 1 ]
   then
     echo "Improper number of arguments supplied"
+    echo "Arguments should be of form :"
+    echo "'bash compile.sh XX'"
     exit 1
 fi
-
-# if [ -d  "$1" ]
-#   then
-#     echo "Folder by this name exists"
-#     echo "Choose a different name"
-#     exit 1
-# fi
 
 if [ $1 = "test_utils" ]; then
     C:/MinGW/bin/g++ utils/$1.cpp -o utils/$1.exe
--- run.sh
+++ run.sh
@@ -1,6 +1,28 @@
-if [ $# -ne 1 ]
+# How to Run :
+# for Python Solution
+# bash run.sh XX py 
+
+# for C++ Solution
+# bash run.sh XX cpp
+
+# where XX => 01, 02, 03 ... 23, 24, 25
+
+# E.g. :
+# 1.
+# bash run.sh 01 cpp
+# executes the C++ solution for Day01
+
+# 2. 
+# bash run.sh 22 py
+# executes the python solution for Day22
+
+if [ $# -ne 2 ]
   then
     echo "Improper number of arguments supplied"
+    echo "Arguments should be of form :"
+    echo "'bash run.sh XX cpp'"
+    echo "or"
+    echo "'bash run.sh XX py'"
     exit 1
 fi
 
@@ -10,5 +32,9 @@ if [ $1 = "test_utils" ];then
 fi
 
 cd Day$1
-./Day$1
+if [ $2 = "cpp" ];then
+    ./Day$1
+else
+    python Day$1.py
+fi
 cd ..
