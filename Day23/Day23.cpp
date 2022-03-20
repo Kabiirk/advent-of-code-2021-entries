c6459a24f3b0efc08cd6a64c5c2b570b234d7ae1
Kabiir <kabiirk@gmail.com>
2022-03-20 20:26:44 +0530

    Added Part 2 input as well

--- Day23/Day23.cpp
+++ Day23/Day23.cpp
@@ -2,8 +2,21 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <map>
+#include <set>
+#include "../utils/utils.h"
 
 using namespace std;
+/*
+Part 2 input
+#############
+#...........#
+###D#A#C#D###
+  #D#C#B#A#
+  #D#B#A#C#
+  #C#A#B#B#
+  #########
+*/
 
 void readFile(string filename){
     string line;
@@ -20,8 +33,18 @@ void readFile(string filename){
 
 }
 
+int checkState(){
+    
+
+    return 0;
+}
+
 int main() {
     readFile("input.txt");
+
+    // Part 1// 14346
+
+    // Part 2// 48984
 
     return 0;
 }
