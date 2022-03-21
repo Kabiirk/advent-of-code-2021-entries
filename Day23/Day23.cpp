c61a074148238ca6cb8ed9fd3ce7177edcada9c6
Kabiir <kabiirk@gmail.com>
2022-03-22 00:09:11 +0530

    Day23.cpp Done

--- Day23/Day23.cpp
+++ Day23/Day23.cpp
@@ -5,20 +5,11 @@
 #include <map>
 #include <array>
 #include <list>
+#include <stdio.h>
 #include <assert.h>
 #include "../utils/utils.h"
 
 using namespace std;
-/*
-Part 2 input
-#############
-#...........#
-###D#A#C#D###
-  #D#C#B#A#
-  #D#B#A#C#
-  #C#A#B#B#
-  #########
-*/
 
 void readFile(string filename){
     string line;
@@ -52,14 +43,15 @@ class Burrow{
     }
 
     void printBurrow(){
+        Print(rooms[0].size());
         cout<<" #############"<<endl;
         cout<<" #";
-        for(unsigned i = 0; i < hallway.size(); i++){
+        for(int i = 0; i < hallway.size(); i++){
             cout<<hallway[i];
         }
         Print("#");
         printf(" ###%c#%c#%c#%c###\n", rooms[0][0], rooms[1][0], rooms[2][0], rooms[3][0]);
-        for(unsigned i = 1; i < rooms[0].size(); ++i){
+        for(int i = 1; i < rooms[0].size(); ++i){
             printf("   #%c#%c#%c#%c#\n", rooms[0][i], rooms[1][i], rooms[2][i], rooms[3][i]);
         }
         printf("   #########\n\n");
@@ -73,7 +65,7 @@ class Burrow{
         return c>='A' && c<='D';
     }
 
-    unsigned targetRoom(char amphipod){
+    int targetRoom(char amphipod){
         return amphipod - 'A';
     }
 
@@ -89,7 +81,7 @@ class Burrow{
                 return false;
             }
         }
-        
+
         return true;
     }
 
@@ -151,23 +143,23 @@ class Burrow{
 
     list<pair<Burrow, int>> moveIn(){
         list<pair<Burrow, int>> result;
-        for (unsigned h = 0; h < hallway.size(); ++h){
+        for (int h = 0; h < hallway.size(); ++h){
             if (isClear(hallway[h])){
                 continue;
             }
             char amphipod = hallway[h];
             // Room the amphipod wants
-            unsigned r = targetRoom(amphipod);
+            int r = targetRoom(amphipod);
             // Room must be empty or only have the same amphipod
             if (!allCozy(r)){
                 continue;
             }
             // Check the steps along the hallway
-            unsigned hr = outsideDoor(r);
+            int hr = outsideDoor(r);
             assert(h != hr);
             int dir = h < hr ? +1 : -1;
-            unsigned hc = h + dir;
-            unsigned steps = 1;
+            int hc = h + dir;
+            int steps = 1;
             while (hc != hr && isClear(hallway[hc])){
                 hc += dir;
                 ++steps;
@@ -177,7 +169,7 @@ class Burrow{
                 continue;
             }
             // Move in
-            unsigned i = 0;
+            int i = 0;
             ++steps;
             // All the way down
             while (i + 1 < rooms[r].size() && isClear(rooms[r][i + 1])) {
@@ -219,7 +211,7 @@ class Burrow{
     }
 };
 
-bool operator<(Burrow& b1, Burrow& b2){
+bool operator<(Burrow const &b1, Burrow const &b2){
     if(b1.hallway < b2.hallway){
         return true;
     }
@@ -249,6 +241,7 @@ pair<int, list<Burrow>> search(Burrow &start){
         auto pt = path_to.find(b);
         assert(pt != path_to.end() && pt->second == to_search.begin());
         path_to.erase(pt);
+        to_search.erase( to_search.begin() );
 
         if(b.allHome()){
             return cb;
@@ -274,6 +267,254 @@ pair<int, list<Burrow>> search(Burrow &start){
     assert(false && "No Solution found");
 }
 
+
+/////////////////////////////////////
+// FOR LONG BURROW
+/////////////////////////////////////
+// TODO : TOO MUCH Code duplication reduce this (e.g. with inheritance, template etc) !!!
+
+class LongBurrow{
+    public:
+    array<char, 11> hallway;
+    array< array<char, 4>, 4 > rooms;
+    map<char, int> move_cost = {
+        {'A',1},
+        {'B',10},
+        {'C',100},
+        {'D',1000},
+    };
+
+    LongBurrow(array<char, 11> h, array< array<char, 4>, 4 > r){
+        hallway = h;
+        rooms = r;
+    }
+
+    void printBurrow(){
+        Print(rooms[0].size());
+        cout<<" #############"<<endl;
+        cout<<" #";
+        for(int i = 0; i < hallway.size(); i++){
+            cout<<hallway[i];
+        }
+        Print("#");
+        printf(" ###%c#%c#%c#%c###\n", rooms[0][0], rooms[1][0], rooms[2][0], rooms[3][0]);
+        for(int i = 1; i < rooms[0].size(); ++i){
+            printf("   #%c#%c#%c#%c#\n", rooms[0][i], rooms[1][i], rooms[2][i], rooms[3][i]);
+        }
+        printf("   #########\n\n");
+    }
+
+    bool isClear(char c){
+        return c=='.';
+    }
+
+    bool isAmphipod(char c){
+        return c>='A' && c<='D';
+    }
+
+    int targetRoom(char amphipod){
+        return amphipod - 'A';
+    }
+
+    int outsideDoor(int r){
+        int end_sz = (hallway.size() - (2*rooms.size()-1) )/2;
+        return 2*r+end_sz;
+    }
+
+    bool allCozy(int r){
+        char amphipod = 'A'+r;
+        for(int i = 0; i<rooms[r].size(); i++){
+            if(!isClear(rooms[r][i]) && rooms[r][i]!=amphipod){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool isForbidden(int h){
+        for(int i=0; i<rooms.size(); i++){
+            if(outsideDoor(i)==h){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    list<pair<LongBurrow, int>> moveOut(){
+        list<pair<LongBurrow, int>> result;
+        for(int r=0; r<rooms.size(); r++){
+            if(allCozy(r)){
+                continue;
+            }
+
+            int i = 0;
+            while(i<rooms[r].size() && isClear(rooms[r][i])){
+                i++;
+            }
+            if(i>=rooms[r].size()){
+                continue;
+            }
+            char amphipod = rooms[r][i];
+            assert(isAmphipod(amphipod));
+
+            // move into hall
+            int steps = i+1;
+            int hall_pos = outsideDoor(r);
+            assert(isClear(hallway[hall_pos]));
+
+            // Walk left or right
+            for(int dir : {-1, +1}){
+                int h = hall_pos+dir;
+                int s = steps+1;
+                while(h>=0 && h<int(hallway.size())){
+                    if(!isClear(hallway[h])){
+                        // someone is in the way
+                        break;
+                    }
+                    if(!isForbidden(h)){
+                        LongBurrow next(*this);
+                        next.rooms[r][i] = '.';
+                        next.hallway[h] = amphipod;
+                        result.emplace_back(next, s*move_cost[amphipod]);
+                    }
+                    h+=dir;
+                    s++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    list<pair<LongBurrow, int>> moveIn(){
+        list<pair<LongBurrow, int>> result;
+        for (int h = 0; h < hallway.size(); ++h){
+            if (isClear(hallway[h])){
+                continue;
+            }
+            char amphipod = hallway[h];
+            // Room the amphipod wants
+            int r = targetRoom(amphipod);
+            // Room must be empty or only have the same amphipod
+            if (!allCozy(r)){
+                continue;
+            }
+            // Check the steps along the hallway
+            int hr = outsideDoor(r);
+            assert(h != hr);
+            int dir = h < hr ? +1 : -1;
+            int hc = h + dir;
+            int steps = 1;
+            while (hc != hr && isClear(hallway[hc])){
+                hc += dir;
+                ++steps;
+            }
+            if (!isClear(hallway[hc])){
+                // Hallway blocked
+                continue;
+            }
+            // Move in
+            int i = 0;
+            ++steps;
+            // All the way down
+            while (i + 1 < rooms[r].size() && isClear(rooms[r][i + 1])) {
+                i++;
+                steps++;
+            }
+            LongBurrow next(*this);
+            next.hallway[h] = '.';
+            next.rooms[r][i] = amphipod;
+            result.emplace_back(next, steps*move_cost[amphipod]);
+        }
+
+        return result;
+    }
+
+    list<pair<LongBurrow, int>> successor(){
+        auto new_state = moveIn();
+        if(!new_state.empty()){
+            return new_state;
+        }
+
+        return moveOut();
+    }
+
+    bool allHome(){
+        for(auto c  : hallway){
+            if(!isClear(c)){
+                return false;
+            }
+        }
+
+        for(int r=0; r<rooms.size(); r++){
+            if(!allCozy(r)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+bool operator<(LongBurrow const &b1, LongBurrow const &b2){
+    if(b1.hallway < b2.hallway){
+        return true;
+    }
+    if(b2.hallway < b1.hallway){
+        return false;
+    }
+
+    return b1.rooms < b2.rooms;
+}
+
+pair<int, list<LongBurrow>> searchLong(LongBurrow &start){
+    multimap<int, list<LongBurrow>> to_search;
+    to_search.emplace(0, list<LongBurrow>(1, start));
+
+    // Pointer from final burrow state to corresponding entry in
+    // to_search
+    map<LongBurrow, decltype(to_search)::iterator> path_to;
+    path_to[start] = to_search.begin();
+
+    while(!to_search.empty()){
+        auto cb = *to_search.begin();
+        int cost = cb.first;
+        list<LongBurrow> &bs = cb.second;
+        LongBurrow &b = bs.back();
+
+        // remove from path_to()
+        auto pt = path_to.find(b);
+        assert(pt != path_to.end() && pt->second == to_search.begin());
+        path_to.erase(pt);
+        to_search.erase( to_search.begin() );
+
+        if(b.allHome()){
+            return cb;
+        }
+        for(auto &next : b.successor()){
+            int next_cost = cost + next.second;
+            auto pt = path_to.find(next.first);
+            if(pt != path_to.end()){
+                if(pt->second->first <= next_cost){
+                    // Already reached this state via a path that's at least as
+                    // good
+                    continue;
+                }
+                to_search.erase(pt->second);
+                path_to.erase(pt);
+            }
+            auto next_bs(bs);
+            next_bs.push_back(next.first);
+            path_to[next.first] = to_search.emplace(next_cost, next_bs);
+        }
+    }
+
+    assert(false && "No Solution found");
+}
+
+
 int main() {
     // readFile("input.txt");
 
@@ -297,9 +538,8 @@ int main() {
         {{'D','B'}}
     }};
     Burrow b(hallway, rooms);
+    // b.printBurrow();
     auto cost_and_steps = search(b);
-    // for(auto &step : cost_and_steps.second){
-    // }
     cout<<cost_and_steps.first<<endl;// 14346
 
     // Part 2// 48984
@@ -312,6 +552,17 @@ int main() {
       #C#A#B#B#
       #########
     */
+    array<char, 11> hallway2 = { '.','.','.','.','.','.','.','.','.','.', '.' };
+    array< array<char, 4>, 4 > rooms2 = {{
+        {{'D', 'D', 'D', 'C'}},
+        {{'A', 'C', 'B', 'A'}},
+        {{'C', 'B', 'A', 'B'}},
+        {{'D', 'A', 'C', 'B'}}
+    }};
+    LongBurrow lb(hallway2, rooms2);
+    // lb.printBurrow();
+    auto cost_and_steps2 = searchLong(lb);
+    cout<<cost_and_steps2.first<<endl;// 48984
 
     return 0;
 }
